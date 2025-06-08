e9091bdc73529e3dd0a7d65ce5e62282b8e8f977
FrankCuiCN <35516663+FrankCuiCN@users.noreply.github.com>
2025-06-07 18:04:33 -0700

    Update utils_openai.py
    Bug fix related to assistant messages

--- src/api/utils_openai.py
+++ src/api/utils_openai.py
@@ -14,20 +14,24 @@ def translate_messages(messages):
         role    = message["role"]
         content = message["content"]
         assert isinstance(content, list), "content must be a list"
+        # Construct content_new
         content_new = []
-        for item in content:
-            if item["type"] == "text":
-                content_new.append({
-                    "type": "input_text",
-                    "text": item["text"],
-                })
-            elif item["type"] == "image":
-                media_type = item["source"]["media_type"]
-                base64_data = item["source"]["data"]
-                content_new.append({
-                    "type": "input_image",
-                    "image_url": f"data:{media_type};base64,{base64_data}",
-                })
+        if role == "user":
+            for item in content:
+                if item["type"] == "text":
+                    content_new.append({"type": "input_text", "text": item["text"]})
+                elif item["type"] == "image":
+                    media_type = item["source"]["media_type"]
+                    base64_data = item["source"]["data"]
+                    content_new.append({"type": "input_image", "image_url": f"data:{media_type};base64,{base64_data}"})
+        elif role == "assistant":
+            content_new = []
+            for item in content:
+                assert item["type"] == "text", "We expect assistant outputs to be text-only"
+                content_new.append({"type": "output_text", "text": item["text"]})
+        else:
+            raise Exception("Unexpected role")
+        # Append the new content to messages_new
         messages_new.append({"role": role, "content": content_new})
     return messages_new
 
@@ -88,11 +92,14 @@ def run(messages, response_mode, parent):
                 logger.debug("The task is halting")
                 # Exit ungracefully
                 return False
+
+            # Debug: refusal is not being handled
             
             if event.type == "response.reasoning_summary_text.delta":
                 parent.signal.emit({"state": "thinking", "payload": None})
             
             if event.type == "response.output_text.delta":
                 parent.signal.emit({"state": "generating", "payload": event.delta})
+
     # Exit gracefully
     return True
